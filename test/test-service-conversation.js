384ee41f8a863e8f14d12a71ab40830a4fded0dc
danpickford <email@danpickford.com>
2016-11-04 17:13:06 +0000

    fished tests ready for submission.

--- test/test-service-conversation.js
+++ test/test-service-conversation.js
@@ -1,7 +1,11 @@
-var app = require('../app');
-var request = require('supertest').agent(app.listen());
+const app = require('../app');
+const request = require('supertest').agent(app.listen());
+const chai = require('chai');
+const should = chai.should();
 
 describe('Conversation API', function () {
+
+    let conversationId = '-';
 
     before(function (done) {
         this.timeout(5000); //for train coding.
@@ -9,84 +13,151 @@ describe('Conversation API', function () {
             done();
         });
     });
-    describe('Setup', function () {
-        it('Delete user James Haywood DEL', function (done) {
-            request.del('/user/James Haywood')
-                .expect(200, function (err, res) {
-                    if (err) return done(err);
-                    done();
-                }).expect('Content-Type', /json/)
-        });
-    });
-    describe('Test CRUD', function () {
-        it('should list all users on /user GET', function (done) {
-            request.get('/user')
-                .expect(200, function (err, res) {
-                    if (err) return done(err);
-                    done();
-                }).expect('Content-Type', /json/);
-        });
-        it('should add one user on /user/add/Dan Pickford POST', function (done) {
-            request.post('/user/add')
+
+    describe('Setup | Ensure users exist', function () {
+        it('Add user James Haywood POST', function (done) {
+            request
+                .post('/user/add')
                 .send({
-                    "username": "Daniel Pickford"
+                    'username': 'James Haywood'
                 })
-                .expect(200, function (err, res) {
+                .expect(200)
+                .end(function (err, res) {
                     if (err) return done(err);
+                    res.should.be.json;
                     done();
-                }).expect('Content-Type', /json/);
-        });
-        it('should add one user on /user/add/James Haywood POST', function (done) {
-            request.post('/user/add')
-                .send({
-                    "username": "James Haywood"
-                })
-                .expect(200, function (err, res) {
-                    if (err) return done(err);
-                    return res.body.username == "James Haywood";
-                    done();
-                }).expect('Content-Type', /json/);
-        });
-        it('should inform user exists on /user/add/James Haywood POST', function (done) {
-            request.post('/user/add')
-                .send({
-                    "username": "James Haywood"
-                })
-                .expect(200, function (err, res) {
-                    if (err) return done(err);
-                    done();
-                }).expect('Content-Type', /json/).expect({
-                    'INFO': 'Username already exists choose another.'
                 });
         });
-        it('should get one user on /user/Joe Fox GET', function (done) {
-            request.get('/user/Joe Fox')
-                .expect(200, function (err, res) {
+        it('Add user Daniel Pickford POST', function (done) {
+            request
+                .post('/user/add')
+                .send({
+                    'username': 'Daniel Pickford'
+                })
+                .expect(200)
+                .end(function (err, res) {
                     if (err) return done(err);
+                    res.should.be.json;
                     done();
-                }).expect('Content-Type', /json/);
+                });
         });
-        it('should delete one user on /user/Daniel Pickford DEL', function (done) {
-            request.del('/user/Daniel Pickford')
-                .expect(200, function (err, res) {
-                    if (err) return done(err);
-                    done();
-                }).expect('Content-Type', /json/)
-                .expect({
-                    "INFO": "Destroyed: 1 Daniel Pickford."
-                });;
-        });
-        describe('Tidy up', function () {
-            it('Delete user James Haywood DEL', function (done) {
-                request.del('/user/James Haywood')
-                    .expect(200, function (err, res) {
+
+
+
+        describe('Test Conversation CRUD', function () {
+            it('should add a new conversation /conversation/add POST', function (done) {
+                request
+                    .post('/conversation/add')
+                    .send({
+                        "from": "Daniel Pickford",
+                        "to": "James Haywood",
+                        "subject": "Technical test tests",
+                        "message": "Hi James, please find technical test at https://gitlab.com/Danpickford/15giftsTechTest.git."
+                    })
+                    .expect(200)
+                    .end(function (err, res) {
                         if (err) return done(err);
+                        res.should.be.json;
+                        res.body[0].should.have.property('subject');
+                        res.body[0].subject.should.equal('Technical test tests');
+                        res.body[0].should.have.property('id');
+                        conversationId = res.body[0].id;
                         done();
-                    }).expect('Content-Type', /json/)
-                    .expect({
-                        "INFO": "Destroyed: 1 James Haywood."
                     });
             });
+
+            it('should get conversation by user on /conversation/James Haywood GET', function (done) {
+                request
+                    .get('/conversation/James Haywood')
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body[0].should.have.property('participants');
+                        res.body[0].should.have.property('messages');
+                        res.body[0].should.have.property('subject');
+                        res.body[0].subject.should.equal('Technical test tests');
+                        done();
+                    });
+            });
+
+            it('should add message to existing conversation PUT' + conversationId, function (done) {
+                request
+                    .put('/conversation')
+                    .send({
+                        "conversationId": conversationId,
+                        "from": "James Haywood",
+                        "to": "Daniel Pickford",
+                        "message": "Ah Dan nice to hear from you, this looks amazing."
+                    })
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body[0].should.have.property('subject')
+                        res.body[0].subject.should.equal('Technical test tests');
+                        done();
+                    });
+            });
+
+            it('should add message to existing conversation PUT', function (done) {
+                request
+                    .put('/conversation')
+                    .send({
+                        "conversationId": conversationId,
+                        "from": "Daniel Pickford",
+                        "to": "James Haywood",
+                        "message": "Thanks James very kind of you to say."
+                    })
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        done();
+                    });
+            });
+
+            it('should delete the conversation', function (done) {
+                request
+                    .del('/conversation/' + conversationId)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body[0].should.have.property('id');
+                        res.body[0].id.should.equal(conversationId)
+                        done();
+                    });
+            });
+
+        });
+    });
+    describe('Tidy up', function () {
+        it('Delete user James Haywood DEL', function (done) {
+            request
+                .del('/user/James Haywood')
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    res.should.be.json;
+                    res.should.be.json;
+                    res.body.should.have.property('INFO');
+                    res.body.INFO.should.equal('Destroyed: 1 James Haywood.');
+                    done();
+                });
+        });
+        it('Delete user Daniel Pickford DEL', function (done) {
+            request
+                .del('/user/Daniel Pickford')
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    res.should.be.json;
+                    res.should.be.json;
+                    res.body.should.have.property('INFO');
+                    res.body.INFO.should.equal('Destroyed: 1 Daniel Pickford.');
+                    done();
+                });
         });
     });
 });
--- test/test-service-users.js
+++ test/test-service-users.js
@@ -1,9 +1,10 @@
-var app = require('../app');
-var request = require('supertest').agent(app.listen());
-var chai = require('chai');
-var should = chai.should();
+const app = require('../app');
+const request = require('supertest').agent(app.listen());
+const chai = require('chai');
+const should = chai.should();
+
 describe('User API', function () {
-    
+
     before(function (done) {
         this.timeout(5000); //for train coding.
         app.__dbInitComplete.then(function () {
@@ -31,9 +32,92 @@ describe('User API', function () {
                     done();
                 });
         });
-    });
-    describe('Test CRUD', function () {
-        it('should list all users on /user GET', function (done) {
+
+        describe('Test User CRUD', function () {
+            it('should add one user on /user/add/Daniel Pickford POST', function (done) {
+                request
+                    .post('/user/add')
+                    .send({
+                        'username': 'Daniel Pickford'
+                    })
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body.should.have.property('username');
+                        res.body.username.should.equal('Daniel Pickford');
+                        this.username = res.body.username;
+                        done();
+                    });
+            });
+
+            it('should add one user on /user/add/James Haywood POST', function (done) {
+                request
+                    .post('/user/add')
+                    .send({
+                        'username': 'James Haywood'
+                    })
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body.should.have.property('username');
+                        res.body.username.should.equal('James Haywood');
+                        this.username = res.body.username;
+                        done();
+                    });
+            });
+
+            it('should inform user exists on /user/add/James Haywood POST', function (done) {
+                request.post('/user/add')
+                    .send({
+                        'username': 'James Haywood'
+                    })
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body.should.have.property('INFO');
+                        res.body.INFO.should.equal('Username already exists choose another.');
+                        done();
+                    });
+            });
+            it('should get one user on /user/James Haywood GET', function (done) {
+                request.get('/user/James Haywood')
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body.should.have.property('username');
+                        res.body.username.should.equal('James Haywood');
+                        done();
+                    });
+            });
+            it('should delete one user on /user/James Haywood DEL', function (done) {
+                request
+                    .del('/user/James Haywood')
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body.should.have.property('INFO');
+                        res.body.INFO.should.equal('Destroyed: 1 James Haywood.');
+                        done();
+                    });
+            });
+            it('should delete one user on /user/Daniel Pickford DEL', function (done) {
+                request
+                    .del('/user/Daniel Pickford')
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        res.should.be.json;
+                        res.body.should.have.property('INFO');
+                        res.body.INFO.should.equal('Destroyed: 1 Daniel Pickford.');
+                        done();
+                    });
+            });
+            it('should list all users on /user GET', function (done) {
             request
                 .get('/user')
                 .expect(200)
@@ -43,85 +127,7 @@ describe('User API', function () {
                     done();
                 });
         });
-        it('should add one user on /user/add/Daniel Pickford POST', function (done) {
-            request
-                .post('/user/add')
-                .send({
-                    'username': 'Daniel Pickford'
-                })
-                .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    res.should.be.json;
-                    res.body.should.have.property('username');
-                    res.body.username.should.equal('Daniel Pickford');
-                    done();
-                });
-        });
 
-        it('should add one user on /user/add/James Haywood POST', function (done) {
-            request
-                .post('/user/add')
-                .send({
-                    'username': 'James Haywood'
-                })
-                .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    newUserId = res.body.id;
-                    res.should.be.json;
-                    res.body.should.have.property('username');
-                    res.body.username.should.equal('James Haywood');
-                    done();
-                });
-        });
-
-        it('should inform user exists on /user/add/James Haywood POST', function (done) {
-            request.post('/user/add')
-                .send({
-                    'username': 'James Haywood'
-                })
-                .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    res.should.be.json;
-                    res.body.should.have.property('INFO');
-                    res.body.INFO.should.equal('Username already exists choose another.');
-                    done();
-                });
-        });
-        it('should get one user on /user/James Haywood GET', function (done) {
-            request.get('/user/James Haywood')
-                .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    res.should.be.json;
-                    res.body.should.have.property('username');
-                    res.body.username.should.equal('James Haywood');
-                    done();
-                });
-        });
-        it('should delete one user on /user/Daniel Pickford DEL', function (done) {
-            request
-                .del('/user/Daniel Pickford')
-                .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    res.should.be.json;
-                    done();
-                });
-        });
-    });
-    describe('Tidy up', function () {
-        it('Delete user James Haywood DEL', function (done) {
-            request
-                .del('/user/James Haywood')
-                .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    res.should.be.json;
-                    done();
-                });
         });
     });
 });
